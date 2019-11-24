0899070890147ca7fe3a3d94cc3ea055dc75e477
Joseh <joseph161216@gmail.com>
2019-11-23 17:17:19 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,21 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		#we can't use strong params here because require(:post) looks for a :post key but there is none in the params. This is because we haven't created a post yet
 	  @post = Post.new
+		# byebug
 	  @post.title = params[:title]
 	  @post.description = params[:description]
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+	# def create
+	#   @post = Post.new
+	#   @post.title = params[:title]
+	#   @post.description = params[:description]
+	#   @post.save
+	#   redirect_to post_path(@post)
+	# end
 
 	def edit
 		@post = Post.find(params[:id])
@@ -25,7 +34,20 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  # @post.update(title: params[:title], description: params[:description])
+	  @post.update(post_params(:title, :description))
 	  redirect_to post_path(@post)
+	end
+	# def update
+	#   @post = Post.find(params[:id])
+	#   @post.update(params.require(:post).permit(:title, :description))
+	#   #.require excapsulates title and description within a post hash
+	#   redirect_to post_path(@post)
+	# end
+
+	private
+
+	def post_params(*args)
+		params.require(:post).permit(*args)
 	end
 end
